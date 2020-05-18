92c2e4b4896e6dcd8113b5ebb6170c9cf1f5f7d3
Agu <agustin.ovari@gmail.com>
2020-05-18 16:39:52 +0200

    SearchFragment n recycler added

---
+++ app/src/main/java/com/ayova/moviseries/adapters/SearchResultsRecyclerAdapter.kt
@@ -0,0 +1,82 @@
+package com.ayova.moviseries.adapters
+
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.ayova.moviseries.R
+import com.ayova.moviseries.models.ItemDetails
+import com.ayova.moviseries.models.ItemDetailsType
+import com.ayova.moviseries.models.SearchedMovie
+import com.ayova.moviseries.models.SearchedTV
+import com.ayova.moviseries.tmdb_api.TmdbApi
+import com.ayova.moviseries.views.ItemDetailsFragment
+import com.squareup.picasso.Picasso
+
+class SearchResultsRecyclerAdapter(private val itemsFound: ArrayList<Any>?, val actContext: Context) : RecyclerView.Adapter<SearchResultsRecyclerAdapter.MainViewHolder>() {
+    private val viewPool = RecyclerView.RecycledViewPool()
+    val TAG = "miappResults"
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.fragment_search_result_recycler, parent, false)
+        return MainViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+        // Get the list
+        val data = itemsFound?.get(position)
+        if (data is SearchedMovie){
+            holder.title.text = data?.title
+            holder.year.text = data?.release_date
+            if (data?.poster_path.isNullOrEmpty()){
+                Picasso.get()
+                    .load("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fnetflixroulette.files.wordpress.com%2F2013%2F01%2Fimage-not-found.gif&f=1&nofb=1")
+                    .resize(50,70).centerCrop().into(holder.poster)
+            } else {
+                Picasso.get().load("${TmdbApi.POSTER_URL}${data?.poster_path}").fit().into(holder.poster)
+            }
+            holder.card.setOnClickListener {
+                Log.v(TAG, "listened to click")
+                holder.activity.supportFragmentManager.beginTransaction().replace(R.id.main_frame_container,
+                    ItemDetailsFragment.newInstance(data.id.toString(),ItemDetailsType.movie.toString()))
+                    .addToBackStack("SearchFragment").commit()
+            }
+        }
+        if (data is SearchedTV){
+            holder.title.text = data?.name
+            holder.year.text = data?.first_air_date
+            if (data?.poster_path.isNullOrEmpty()){
+                Picasso.get()
+                    .load("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Felitescreens.com%2Fimages%2Fproduct_album%2Fno_image.png&f=1&nofb=1")
+                    .into(holder.poster)
+            } else {
+                Picasso.get().load("${TmdbApi.POSTER_URL}${data?.poster_path}").fit().into(holder.poster)
+            }
+
+            holder.card.setOnClickListener {
+                Log.v(TAG, "listened to click")
+                holder.activity.supportFragmentManager.beginTransaction().replace(R.id.main_frame_container,
+                    ItemDetailsFragment.newInstance(data.id.toString(),ItemDetailsType.tv_show.toString()))
+                    .addToBackStack("SearchFragment").commit()
+            }
+        }
+    }
+    override fun getItemCount(): Int {
+        return itemsFound?.size ?: 0
+    }
+    inner class MainViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+        val title: TextView = v.findViewById(R.id.search_result_recycler_title)
+        val year: TextView = v.findViewById(R.id.search_result_recycler_year)
+        val poster: ImageView = v.findViewById(R.id.search_result_recycler_poster)
+        val activity: AppCompatActivity = v.context as AppCompatActivity
+        val card: CardView = v.findViewById(R.id.search_result_recycler_card)
+    }
+}
--- app/src/main/java/com/ayova/moviseries/views/SearchFragment.kt
+++ app/src/main/java/com/ayova/moviseries/views/SearchFragment.kt
@@ -1,11 +1,23 @@
 package com.ayova.moviseries.views
 
-import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.*
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ayova.moviseries.R
+import com.ayova.moviseries.adapters.HomeListsRecycler
+import com.ayova.moviseries.adapters.SearchResultsRecyclerAdapter
+import com.ayova.moviseries.models.SearchedMovie
+import com.ayova.moviseries.models.SearchedMovies
+import com.ayova.moviseries.models.SearchedTV
+import com.ayova.moviseries.models.SearchedTVs
+import com.ayova.moviseries.tmdb_api.TmdbApi
 import kotlinx.android.synthetic.main.fragment_search.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 /**
  * A simple [Fragment] subclass.
@@ -13,6 +25,10 @@ import kotlinx.android.synthetic.main.fragment_search.*
 class SearchFragment : Fragment() {
 
     private val TAG = "miapp"
+    val searchedMovies = ArrayList<SearchedMovie>()
+    val searchedTVs = ArrayList<SearchedTV>()
+    var listOfItems: ArrayList<Any> = ArrayList()
+    lateinit var recyclerAdapter: SearchResultsRecyclerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,9 +41,79 @@ class SearchFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        search_btn_opensearch.setOnClickListener {
-            startActivity(Intent(activity!!.applicationContext, SearchActivity::class.java))
-        }
+        // Recycler
+        val layoutManager = LinearLayoutManager(activity!!.applicationContext)
+        search_frag_recycler.layoutManager = layoutManager
+        recyclerAdapter = SearchResultsRecyclerAdapter(listOfItems, activity!!.applicationContext)
+        search_frag_recycler.adapter = recyclerAdapter
+
+        (search_frag_searchview as SearchView).setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (!query.isNullOrBlank()){
+                    Log.v(TAG, query)
+                    searchedMovie(query)
+//                    searchedTV(query)
+                    listOfItems.clear()
+                    listOfItems.addAll(searchedTVs)
+                    listOfItems.addAll(searchedMovies)
+                    return true
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+        })
+    }
+
+    /**
+     * Functions for searching movie/tv show with string provided
+     */
+    private fun searchedMovie(query: String) {
+        val call = TmdbApi.service.searchMovie(query = query)
+        call.enqueue(object : Callback<SearchedMovies> {
+            override fun onResponse(call: Call<SearchedMovies>, response: Response<SearchedMovies>) {
+                val body = response.body()
+                if (response.isSuccessful && body != null) {
+                    searchedMovies.clear()
+                    searchedMovies.addAll(body.results)
+                    listOfItems.clear()
+                    listOfItems.addAll(body.results)
+                    searchedTV(query)
+                    // Imprimir aqui el listado
+                    Log.v(TAG, searchedMovies.toString())
+                    recyclerAdapter?.notifyDataSetChanged()
+                } else {
+                    Log.e(TAG, response.errorBody()!!.string())
+                }
+            }
+            override fun onFailure(call: Call<SearchedMovies>, t: Throwable) {
+                Log.e(TAG, t.message!!)
+            }
+        })
+    }
+
+    private fun searchedTV(query: String) {
+        val call = TmdbApi.service.searchTV(query = query)
+        call.enqueue(object : Callback<SearchedTVs> {
+            override fun onResponse(call: Call<SearchedTVs>, response: Response<SearchedTVs>) {
+                val body = response.body()
+                if (response.isSuccessful && body != null) {
+                    searchedTVs.clear()
+                    searchedTVs.addAll(body.results)
+                    listOfItems.addAll(body.results)
+                    // Imprimir aqui el listado
+                    Log.v(TAG, searchedTVs.toString())
+                    recyclerAdapter?.notifyDataSetChanged()
+                } else {
+                    Log.e(TAG, response.errorBody()!!.string())
+                }
+            }
+            override fun onFailure(call: Call<SearchedTVs>, t: Throwable) {
+                Log.e(TAG, t.message!!)
+            }
+        })
     }
 }
 
